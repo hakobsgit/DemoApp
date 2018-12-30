f29c7c775f0101e98253d2a6a928f26e1dddc2cb
hakobsgit <hakob.babayan@icloud.com>
2018-12-30 19:00:13 +0400

    new commit

---
+++ app/src/main/java/com/my/hbr/demoapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.my.hbr.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.my.hbr.demoapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/my/hbr/demoapp/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.my.hbr.demoapp.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
